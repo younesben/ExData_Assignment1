70d44ec059aaedbaa20ecc861e09253ed984664f
younesben <mohamedbenkhemis@yahoo.fr>
2014-05-12 02:21:55 +0200

    Assignment1

--- plot4.R
+++ plot4.R
@@ -9,10 +9,14 @@ dataTable$Sub_metering_2<-as.numeric(levels(dataTable$Sub_metering_2))[dataTable
 dataTable$Sub_metering_3<-as.numeric(levels(dataTable$Sub_metering_3))[dataTable$Sub_metering_3]
 dataTable$Voltage<-as.numeric(levels(dataTable$Voltage))[dataTable$Voltage]
 dataTable2 <- subset(dataTable, Date >= as.Date("2007-02-01")  & Date <= as.Date("2007-02-02"))
-png(file="plot3.png")
-plot(dataTable2$Time,dataTable2$Sub_metering_1,type="l",main="",ylab="Energy sub metering",xlab="")
-lines(dataTable2$Time,dataTable2$Sub_metering_2,type="l",col="red")
-lines(dataTable2$Time,dataTable2$Sub_metering_3,type="l",col="blue")
-legend("topright", col = c("black", "red","blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=1)
+png(file="plot4.png")
+par(mfrow = c(2, 2))
+with(dataTable2, plot(Time,Global_active_power,type="l",main="",ylab="Global active Power",xlab=""))
+with(dataTable2,  plot(Time,Voltage,type="l",xlab="datetime"))
+with(dataTable2,plot(Time,Sub_metering_1,type="l",main="",ylab="Energy sub metering",xlab="") )
+with(dataTable2,lines(Time,Sub_metering_2,type="l",col="red"))
+with(dataTable2,lines(Time,Sub_metering_3,type="l",col="blue"))
+with(dataTable2,legend("topright", col = c("black", "red","blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=1))
+with(dataTable2, plot(Time,Global_reactive_power,type="l",main="",xlab="datetime"))
 dev.off()
 
